30a8db5c6a46ec700d2d14fa41574d58044e8f71
Kersie30 <keresiakaruma@gmail.com>
2024-12-10 15:12:17 +0200

    added typewriterplugin and basic form handling

--- SRC/index.js
+++ SRC/index.js
@@ -1,1 +1,14 @@
+function generatePoem(event) {
+  event.preventDefault();
 
+  new Typewriter("#poem", {
+    strings:
+      "4 (4 ounce) lamb chops 1 tablespoon olive oil, or as needed 1 clove",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let poemFormElement = document.querySelector("#poem-generator-form");
+poemFormElement.addEventListener("submit", generatePoem);
