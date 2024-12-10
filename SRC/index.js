b9698ebee5e8657facabbea00430fdf23007f5b5
Kersie30 <keresiakaruma@gmail.com>
2024-12-10 16:14:24 +0200

    added the AI api intergration and more JS

--- SRC/index.js
+++ SRC/index.js
@@ -1,14 +1,28 @@
-function generatePoem(event) {
-  event.preventDefault();
-
-  new Typewriter("#poem", {
-    strings:
-      "4 (4 ounce) lamb chops 1 tablespoon olive oil, or as needed 1 clove",
+function displayRecipe(response) {
+  new Typewriter("#recipe", {
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
 }
 
-let poemFormElement = document.querySelector("#poem-generator-form");
-poemFormElement.addEventListener("submit", generatePoem);
+function generateRecipe(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "596e8ff92tc946d86a0032fef5649ob0";
+  let prompt = `User instructions: Generate a recipe about ${instructionsInput.value}`;
+  let context =
+    "You are a recipe expert and love to write short recipes. Your mission is to generate an 8 line recipe. Each line must be seperated with a <br/>. Make sure to follow the user instructions. Do not include a title to the recipe. Sign the recipe with 'SheCodes AI' inside a strong <strong> element at the bottom of the recipe";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  let recipeElement = document.querySelector("#recipe");
+  recipeElement.classList.remove("hidden");
+  recipeElement.innerHTML = `<div class="generating">🖋 A recipe about ${instructionsInput.value} is generating...</div>`;
+
+  axios.get(apiURL).then(displayRecipe);
+}
+
+let recipeFormElement = document.querySelector("#recipe-generator-form");
+recipeFormElement.addEventListener("submit", generateRecipe);
